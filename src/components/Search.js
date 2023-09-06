d3a6bd0cb51359fe387862c27f43e116af5f1f3d
Jason <jconn99801@gmail.com>
2023-09-06 13:17:32 -0800

    updated search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 function Search({ searchInput, setSearchInput }) {
-  <div className="ui large fluid icon input">
-    <input
-      value={searchInput}
-      type="text"
-      placeholder="Search Recipe Names and Ingredients..."
-      onChange={(e) => setSearchInput(e.target.value)}
-    />
-    <i className="circular search link icon"></i>
+  const handleOnChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  <div className="ui search">
+    <div className="ui icon input">
+      <input
+        className="prompt"
+        value={searchInput}
+        type="text"
+        placeholder="Search Recipe Names and Ingredients..."
+        onChange={handleOnChange}
+      />
+      <i className="search icon"></i>
+    </div>
   </div>;
 }
 
