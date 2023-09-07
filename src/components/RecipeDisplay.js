8282ae7d8cb34c485d4ed500a14a8a4530080270
Jason <jconn99801@gmail.com>
2023-09-06 22:07:30 -0800

    updated Recipe Display for onclick

--- src/components/RecipeDisplay.js
+++ src/components/RecipeDisplay.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-
-function RecipeDisplay({toggleDisplay}) {
-    return ()
+function RecipeDisplay({ recipe, onClose }) {
+  return (
+    <div>
+      <h2>{recipe.name}</h2>
+      <img src={recipe.image} alt={recipe.name} />
+      <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+      <p>Instructions: {recipe.instructions}</p>
+      <button onClick={onClose}>Close</button>{" "}
+    </div>
+  );
 }
 
 export default RecipeDisplay;
