995b4300db84a1d5481734d5f1704f75def1073a
Jason <jconn99801@gmail.com>
2023-09-06 17:19:42 -0800

    styled search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,16 +6,18 @@ function Search({ searchInput, setSearchInput }) {
   };
   return (
     <div>
-      <h2>Search function...</h2>
       <form className="app-form">
-        <label>Search Recipes</label>
-        <input
-          className="prompt"
-          value={searchInput}
-          type="text"
-          placeholder="Search Recipe Names and Ingredients..."
-          onChange={handleOnChange}
-        />
+        <div className="input-group mb-3">
+          <label className="form-label" htmlFor="searchBar"></label>
+          <input
+            id="searchBar"
+            className="form-control form-control-lg"
+            value={searchInput}
+            type="text"
+            placeholder="Search Recipe Names and Ingredients..."
+            onChange={handleOnChange}
+          />
+        </div>
       </form>
     </div>
   );
