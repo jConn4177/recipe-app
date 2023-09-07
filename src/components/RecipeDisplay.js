f0761087f14c4b5ee82b77e3d5e44507edbd3a32
Jason <jconn99801@gmail.com>
2023-09-07 01:02:54 -0800

    formatted recipeDisplay

--- src/components/RecipeDisplay.js
+++ src/components/RecipeDisplay.js
@@ -2,12 +2,38 @@ import React from "react";
 
 function RecipeDisplay({ recipe, onClose }) {
   return (
-    <div>
-      <h2>{recipe.name}</h2>
-      <img src={recipe.image} alt={recipe.name} />
-      <p>Ingredients: {recipe.ingredients.join(", ")}</p>
-      <p>Instructions: {recipe.instructions}</p>
-      <button onClick={onClose}>Close</button>{" "}
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <img src={recipe.image} alt={recipe.name} className="img-fluid" />
+          <h2>{recipe.name}</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6 col-md-6">
+          <div className="ingredient-box">
+            <h3>Ingredients:</h3>
+            <ul className="list-unstyled">
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6">
+          <div className="instruction-box">
+            <h3>Instructions:</h3>
+            <p>{recipe.instructions}</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <button onClick={onClose} className="btn btn-light">
+            Close
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
