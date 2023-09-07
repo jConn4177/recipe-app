6e81584d11fdb3f7299da6c6c589b6d286c9ce4f
Jason <jconn99801@gmail.com>
2023-09-07 13:18:16 -0800

    updated display aesthetics

--- src/components/RecipeDisplay.js
+++ src/components/RecipeDisplay.js
@@ -5,13 +5,20 @@ function RecipeDisplay({ recipe, onClose }) {
     <div className="container">
       <div className="row">
         <div className="col-lg-12">
-          <img src={recipe.image} alt={recipe.name} className="img-fluid" />
-          <h2>{recipe.name}</h2>
+          <img
+            src={recipe.image}
+            alt={recipe.name}
+            className="img-fluid image-border"
+          />
+          <h2 className="p-3">{recipe.name}</h2>
         </div>
       </div>
       <div className="row">
-        <div className="col-lg-3 col-md-3">
-          <div className="ingredient-box">
+        <div
+          id="ingredients-div-container"
+          className="col-lg-3 col-md-3 custom-border m-4"
+        >
+          <div className="ingredient-box bg-white">
             <h3>Ingredients:</h3>
             <ul className="list-unstyled">
               {recipe.ingredients.map((ingredient, index) => (
@@ -20,22 +27,27 @@ function RecipeDisplay({ recipe, onClose }) {
             </ul>
           </div>
         </div>
-        <div className="col-lg-6 col-md-6">
-          <div className="instruction-box">
-            <h3>Instructions:</h3>
+        <div
+          id="instructions-div-container"
+          className="container-fluid col-lg-6 col-md-6 custom-border m-4"
+        >
+          <div className="row">
+            <div className="col-6">
+              <h3>Instructions:</h3>
+            </div>
+            <div className="col-6 text-end">
+              <button onClick={onClose} className="btn btn-light">
+                Close Recipe
+              </button>
+            </div>
+          </div>
+          <div className="instruction-box bg-white">
             <ol>
               {recipe.instructions.map((instruction, index) => (
                 <li key={index}>{instruction}</li>
               ))}
             </ol>
           </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <button onClick={onClose} className="btn btn-light">
-            Close
-          </button>
         </div>
       </div>
     </div>
